9097a6c5bd47c9c63f87f71f6c8c60da414b01dc
GRASiN1 <gauravrajsingh047@gmail.com>
2024-10-24 10:43:29 +0530

    changeed the orign location

--- config/cors.js
+++ config/cors.js
@@ -1,8 +1,7 @@
 const corsOptions = {
-    origin: 'http://example.com', // Allow only this origin
-    methods: ['GET', 'POST'], // Allow only specific HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow only specific headers
-    credentials: true // Allow cookies and authorization headers with the requests
+  origin: "http://localhost:3000", // Allow only this originy specific HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow only specific headers
+  credentials: true, // Allow cookies and authorization headers with the requests
 };
 
 module.exports = corsOptions;
