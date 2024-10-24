bedbdda6103f5378c465f1785ec95adc25feeccb
GRASiN1 <gauravrajsingh047@gmail.com>
2024-10-24 10:44:01 +0530

    no changes idk why it needs commit

--- controller/user.js
+++ controller/user.js
@@ -1,75 +1,76 @@
-const User = require('../models/user');
-const { generateToken } = require('../services/authentication');
+const User = require("../models/user");
+const { generateToken } = require("../services/authentication");
 
 async function handleCreateUser(req, res) {
-    const { fullName, email, password } = req.body;
-    const userExists = await User.findOne({ email: email });
+  const { fullName, email, password } = req.body;
+  const userExists = await User.findOne({ email: email });
 
-    if (userExists) {
-        res.status(400);
-        throw new Error('User already exists');
-    }
-    const user = await User.create({
-        fullName,
-        email,
-        password,
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.fullName,
+      email: user.email,
+      token: generateToken({
+        fullName: user.fullName,
+        email: user.email,
+        _id: user._id,
+        role: user.role,
+      }),
     });
-    if (user) {
-        res.status(201).json({
-            _id: user._id,
-            name: user.fullName,
-            email: user.email,
-            token: generateToken({
-                fullName: user.fullName,
-                email: user.email,
-                _id: user._id,
-                role: user.role,
-            }),
-        });
-    } else {
-        res.status(400);
-        throw new Error('Invalid user data');
-    }
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 }
 
 async function handleUserLogin(req, res) {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email: email });
-    if (user && user.matchPassword(password)) {
-        return res.json({
-            _id: user._id,
-            name: user.fullName,
-            email: user.email,
-            token: generateToken({
-                fullName: user.fullName,
-                email: user.email,
-                _id: user._id,
-                role: user.role,
-            }),
-        })
-    } else {
-        res.status(401);
-        throw new Error('Invalid email or password');
-    }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+
+  if (user && user.matchPassword(password)) {
+    // Create a new object without the password
+    const { password: userPassword, ...userWithoutPassword } = user.toObject();
+    return res.json({
+      user: userWithoutPassword, // Send user data without password
+      token: generateToken({
+        fullName: user.fullName,
+        email: user.email,
+        _id: user._id,
+        role: user.role,
+      }),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 }
 
 async function handleGetProfile(req, res) {
-    const user = await User.findById({ _id: req.user._id });
-    if (user) {
-        res.json({
-            _id: user._id,
-            name: user.fullName,
-            email: user.email,
-            role: user.role,
-        });
-    } else {
-        res.status(404);
-        throw new Error('User not found');
-    }
+  const user = await User.findById({ _id: req.user._id });
+  if (user) {
+    res.json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      role: user.role,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 }
 
 module.exports = {
-    handleCreateUser,
-    handleGetProfile,
-    handleUserLogin,
-}
+  handleCreateUser,
+  handleGetProfile,
+  handleUserLogin,
+};
--- models/user.js
+++ models/user.js
@@ -1,43 +1,46 @@
-const { Schema, model } = require('mongoose');
-const { genSalt, hash, compare } = require('bcryptjs');
+const { Schema, model } = require("mongoose");
+const { genSalt, hash, compare } = require("bcryptjs");
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     fullName: {
-        required: true,
-        type: String,
+      required: true,
+      type: String,
     },
     email: {
-        required: true,
-        type: String,
-        unique: true,
+      required: true,
+      type: String,
+      unique: true,
     },
     password: {
-        required: true,
-        type: String,
+      required: true,
+      type: String,
     },
     role: {
-        type: String,
-        default: 'NORMAL',
-    }
-}, {
+      type: String,
+      default: "NORMAL",
+    },
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await compare(enteredPassword, this.password);
-}
+  return await compare(enteredPassword, this.password);
+};
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next();
-    }
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-    const salt = await genSalt(5);
-    const hashedPassword = await hash(this.password, salt);
+  const salt = await genSalt(5);
+  const hashedPassword = await hash(this.password, salt);
 
-    this.password = hashedPassword;
-})
+  this.password = hashedPassword;
+});
 
-const User = model('users', userSchema);
+const User = model("users", userSchema);
 
 module.exports = User;
