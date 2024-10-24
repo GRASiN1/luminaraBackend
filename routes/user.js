3a0047ca429af752d6d3bbe7c6e7006f28f8d6f5
GRASiN1 <gauravrajsingh047@gmail.com>
2024-10-24 09:49:02 +0530

    some error

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,15 @@
-const { Router } = require('express');
-const { handleCreateUser, handleUserLogin, handleGetProfile, } = require('../controller/user');
-const { authenticateUser } = require('../middleware/authentication');
+const { Router } = require("express");
+const {
+  handleCreateUser,
+  handleUserLogin,
+  handleGetProfile,
+} = require("../controller/user");
+const { authenticateUser } = require("../middleware/authentication");
 
 const router = Router();
 
-router.post('/createUser', handleCreateUser);
-router.post('/loginUser', handleUserLogin);
-router.get('/getUserDetails', authenticateUser, handleGetProfile);
+router.post("/createUser", handleCreateUser);
+router.post("/loginUser", handleUserLogin);
+router.get("/getUserDetails", authenticateUser, handleGetProfile);
 
 module.exports = router;
