e1442a406977bb1856012b6c22378e999f7cbd87
GRASiN1 <gauravrajsingh047@gmail.com>
2024-10-27 16:15:49 +0530

    changed the way we handled getProducts

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,35 @@
-const { Router } = require('express');
-const { handleGetAllProducts, handleGetProductById, handleCreateProduct, handleUpdateProductById, handleDeleteProductById } = require('../controller/product');
-const { restrictTo } = require('../middleware/authorization');
+const { Router } = require("express");
+const {
+  handleGetAllProducts,
+  handleGetProductById,
+  handleCreateProduct,
+  handleUpdateProductById,
+  handleDeleteProductById,
+} = require("../controller/product");
+const { restrictTo } = require("../middleware/authorization");
 
 const router = Router();
 
-router.post('/createProduct', restrictTo(['ADMIN']), handleCreateProduct);
-router.post('/updateProductById/:id', restrictTo(['ADMIN']), handleUpdateProductById);
-router.delete('/deleteProductById/:id', restrictTo(['ADMIN']), handleDeleteProductById);
-router.get('/getAllProducts', restrictTo(['NORMAL', 'ADMIN']), handleGetAllProducts);
-router.get('/getProductById/:id', restrictTo(['NORMAL', 'ADMIN']), handleGetProductById);
+router.post("/createProduct", restrictTo(["ADMIN"]), handleCreateProduct);
+router.post(
+  "/updateProductById/:id",
+  restrictTo(["ADMIN"]),
+  handleUpdateProductById
+);
+router.delete(
+  "/deleteProductById/:id",
+  restrictTo(["ADMIN"]),
+  handleDeleteProductById
+);
+router.get(
+  "/getAllProducts",
+  restrictTo(["NORMAL", "ADMIN"]),
+  handleGetAllProducts
+);
+router.get(
+  "/getProductById/:id",
+  restrictTo(["NORMAL", "ADMIN"]),
+  handleGetProductById
+);
 
 module.exports = router;
