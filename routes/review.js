5dce76cfc0308582834cc835977354e2f092f5e2
GRASiN1 <gauravrajsingh047@gmail.com>
2024-11-08 12:37:40 +0530

    formating changes

--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,12 @@
-const { Router } = require('express');
-const { handleCreateReview, handleGetAllReViewsOfProduct } = require('../controller/review');
+const { Router } = require("express");
+const {
+  handleCreateReview,
+  handleGetAllReViewsOfProduct,
+} = require("../controller/review");
 
 const router = Router();
 
-router.post('/createReview/:productId', handleCreateReview);
-router.get('/getAllReviewsOfProduct/:productId', handleGetAllReViewsOfProduct);
+router.post("/createReview/:productId", handleCreateReview);
+router.get("/getAllReviewsOfProduct/:productId", handleGetAllReViewsOfProduct);
 
 module.exports = router;
