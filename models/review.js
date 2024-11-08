ec7bc504791782d0b2c4d5ee1d9c1a6c6921eb43
GRASiN1 <gauravrajsingh047@gmail.com>
2024-11-08 12:38:05 +0530

    formating changes

--- models/review.js
+++ models/review.js
@@ -1,30 +1,33 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const reviewSchema = new Schema({
+const reviewSchema = new Schema(
+  {
     reviewBy: {
-        required: true,
-        type: Schema.Types.ObjectId,
-        ref: 'users',
+      required: true,
+      type: Schema.Types.ObjectId,
+      ref: "users",
     },
     reviewBody: {
-        required: true,
-        type: String,
+      required: true,
+      type: String,
     },
     stars: {
-        required: true,
-        type: Number,
-        min: 0,
-        max: 5,
+      required: true,
+      type: Number,
+      min: 0,
+      max: 5,
     },
     reviewOf: {
-        required: true,
-        type: Schema.Types.ObjectId,
-        ref: 'products',
-    }
-}, {
+      required: true,
+      type: Schema.Types.ObjectId,
+      ref: "products",
+    },
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-const Review = model('reviews', reviewSchema);
+const Review = model("reviews", reviewSchema);
 
 module.exports = Review;
