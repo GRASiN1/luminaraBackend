c9043937b8c2c058a89d8f47dda24dd88a30907e
GRASiN1 <gauravrajsingh047@gmail.com>
2024-11-08 12:39:09 +0530

    changed what would happen if user already reviewed the product and some formating changes

--- controller/review.js
+++ controller/review.js
@@ -1,41 +1,45 @@
-const Review = require('../models/review');
-const Product = require('../models/product');
+const Review = require("../models/review");
+const Product = require("../models/product");
 
 async function handleCreateReview(req, res) {
-    const { rating, comment } = req.body;
-    const product = await Product.findById(req.params.productId);
-    if (product) {
-        const reviewsOfTheProduct = await Review.find({ reviewOf: req.params.productId });
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.productId);
+  if (product) {
+    const reviewsOfTheProduct = await Review.find({
+      reviewOf: req.params.productId,
+    });
 
-        const alreadyReviewed = reviewsOfTheProduct.find(
-            (r) => r.reviewBy.toString() === req.user._id.toString()
-        );
-        if (alreadyReviewed) {
-            return res.status(400).json({ message: 'Already reviewed by current user' });
-        }
-        await Review.create({
-            reviewBy: req.user._id,
-            reviewBody: comment,
-            stars: rating,
-            reviewOf: req.params.productId,
-        })
-        return res.status(200).json({ message: 'review added' });
-    } else {
-        return res.status(400).json({ message: 'No product by this id' });
+    const alreadyReviewed = reviewsOfTheProduct.find(
+      (r) => r.reviewBy.toString() === req.user._id.toString()
+    );
+    if (alreadyReviewed) {
+      return res
+        .status(200)
+        .json({ message: "Already reviewed by current user" });
     }
+    const review = await Review.create({
+      reviewBy: req.user._id,
+      reviewBody: comment,
+      stars: rating,
+      reviewOf: req.params.productId,
+    });
+    return res.status(200).json({ message: "review added", review: review });
+  } else {
+    return res.status(400).json({ message: "No product by this id" });
+  }
 }
 
 async function handleGetAllReViewsOfProduct(req, res) {
-    const productId = req.params.productId;
-    const reviewList = await Review.find({ reviewOf: productId });
-    if (reviewList.length !== 0) {
-        return res.status(200).json(reviewList);
-    } else {
-        return res.status(400).json({ message: 'No reviews on this product' });
-    }
+  const productId = req.params.productId;
+  const reviewList = await Review.find({ reviewOf: productId });
+  if (reviewList.length !== 0) {
+    return res.status(200).json(reviewList);
+  } else {
+    return res.status(400).json({ message: "No reviews on this product" });
+  }
 }
 
 module.exports = {
-    handleCreateReview,
-    handleGetAllReViewsOfProduct
-}
+  handleCreateReview,
+  handleGetAllReViewsOfProduct,
+};
